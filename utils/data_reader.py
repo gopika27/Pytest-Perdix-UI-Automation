e4a57f52c06320e82896b4193eeda8e40c321f26
Gopika Dethan <gopika.d@thinkpalm.com>
2026-09-10 12:25:36 +0530

    Handle credentials securely in CI

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -4,15 +4,19 @@ import os
 
 def get_login_data():
 
-    # CI/CD environment
     if os.getenv("CI") == "true":
-
-        return {
+        data = {
             "account_number": os.getenv("ACCOUNT_NUMBER"),
             "username": os.getenv("LOGIN_USERNAME"),
             "password": os.getenv("LOGIN_PASSWORD")
         }
 
-    # Local execution
+        print("CI credentials loaded:")
+        print("Account number present:", bool(data["account_number"]))
+        print("Username present:", bool(data["username"]))
+        print("Password present:", bool(data["password"]))
+
+        return data
+
     with open("test_data/login_data.json") as f:
         return json.load(f)
